6ad9cff04529cbcc874c4a21f964cc65f0265b4e
Attila Bagossy <bagossyattila@outlook.com>
2018-08-15 22:11:23 +0200

    Created a command to list available features

---
+++ src/cmd/list.js
@@ -0,0 +1,20 @@
+const { readFeatures } = require('../features');
+
+module.exports = {
+    command: 'list',
+    desc: 'List available features.',
+    handler() {
+        readFeatures()
+            .then(features => {
+                console.log('Available features:\n');
+
+                features
+                    .map(f => f.keys.join(' ') + '\ttree-ish: ' + f.data.treeish)
+                    .forEach(s => console.log('\t' + s));
+            })
+            .catch(err => {
+                console.log('Could not read the list of available features:');
+                console.log(err);
+            });
+    }
+};
