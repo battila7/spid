c09189dfd163c33bfd5e09056b9b3efb9485184e
Attila Bagossy <bagossyattila@outlook.com>
2018-08-15 22:06:23 +0200

    Added feature extraction to features.js

--- src/features.js
+++ src/features.js
@@ -11,6 +11,41 @@ function readFeaturesFile() {
     });
 }
 
+function isContainerObject(obj) {
+    return Object.keys(obj).every(k => obj[k] !== null && (typeof obj[k] == 'object'));
+}
+
+function isFeatureObject(obj) {
+    if (Object.keys(obj).length > 1) {
+        return false;
+    }
+
+    return obj.location && (typeof obj.location == 'string');
+}
+
+function extractFeatures(obj) {
+    function inner(obj, keys) {
+        for (let key in obj) {
+            if (isContainerObject(obj[key])) {
+                return inner(obj[key], keys.concat([key]));
+            } else if (isFeatureObject(obj[key])) {
+                return result.push({
+                    keys: keys.concat(key),
+                    data: obj[key]
+                });
+            } else {
+                throw new Error('Erroneous contents, the provided file is not a well-formed features file!');í
+            }
+        }
+    }
+
+    var result = [];
+
+    inner(obj, []);
+
+    return result;
+}
+
 function readFeatures() {
     return readFeaturesFile()
         .then(downson)
@@ -27,7 +62,8 @@ function readFeatures() {
             }
 
             return data.features;
-        });
+        })
+        .then(extractFeatures)
 }
 
 module.exports = {
