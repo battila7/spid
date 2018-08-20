a084b2c71b0b6fb9f521a2cfad6c9eba482126a7
Attila Bagossy <bagossyattila@outlook.com>
2018-08-20 10:37:47 +0200

    Refactored features.js to use the abstracted downson reading facilities

--- src/features.js
+++ src/features.js
@@ -1,15 +1,6 @@
-const { readFile } = require('fs');
-
-const downson = require('downson');
+const { readDownson } = require('./downson');
 
 const FEATURES_FILENAME = 'features.spid';
-const UTF8_ENCODING = 'utf8';
-
-function readFeaturesFile() {
-    return new Promise((resolve, reject) => {
-        readFile(FEATURES_FILENAME, { encoding: UTF8_ENCODING }, (err, data) => err ? reject(err) : resolve(data));
-    });
-}
 
 function isContainerObject(obj) {
     return Object.keys(obj).every(k => obj[k] !== null && (typeof obj[k] == 'object'));
@@ -47,15 +38,7 @@ function extractFeatures(obj) {
 }
 
 function readFeatures() {
-    return readFeaturesFile()
-        .then(downson)
-        .then(({ data, hasInterpretationErrors }) => {
-            if (hasInterpretationErrors) {
-                throw new Error('The features file cannot be interpreted correctly!');
-            }
-
-            return data;
-        })
+    return readDownson(FEATURES_FILENAME)
         .then(data => {
             if (!data.features) {
                 throw new Error('The features file has no top-level features key!');
