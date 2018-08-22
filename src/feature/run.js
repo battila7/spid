aceef6ec998ee64b21789b8b7af428ed995f485d
Attila Bagossy <bagossyattila@outlook.com>
2018-08-22 17:39:29 +0200

    Reading the appropriate fixtures file and running its contents

--- src/feature/run.js
+++ src/feature/run.js
@@ -4,14 +4,27 @@ const checkout = require('./steps/checkout');
 
 const { runFixture } = require('../fixture/run');
 
+const { readFixtures } = require('../fixture/read');
+
+function runFixtures({ description, fixtures }, feature, baseOptions) {
+    const runOpts = Object.assign({}, baseOptions, description);
+
+    function reduceCallback(acc, curr) {
+        return acc.then(() => runFixture(curr, feature, runOpts));
+    }
+
+    return fixtures.reduce(reduceCallback, Promise.resolve());
+}
+
 function runFeature(feature, options) {
-    console.log('\Benchmarking feature: ' + feature.keys.join(' ') + '\n');
+    console.log('\nBenchmarking feature: ' + feature.keys.join(' ') + '\n');
 
     const finallyFunc = () => cleanup(options.checkoutDirectory);
 
     return clone(options.remote, options.checkoutDirectory)
         .then(() => checkout(options.remote, feature.data.treeish, options.checkoutDirectory))
-        .then(() => runFixture())
+        .then(() => readFixtures(path.resolve(options.checkoutDirectory, feature.fixturesFile)))
+        .then(data => runFixtures(data, options))
         .then(() => finallyFunc().then(true))
         .catch(err => {
             console.log('Error benchmarking feature');
--- src/fixture/run.js
+++ src/fixture/run.js
@@ -1,5 +1,13 @@
-function runFixture(fixtureFile, checkoutDirectory) {
-    
+const { getRunner } = require('../runner');
+
+function runFixture(fixture, feature, options) {
+    const runner = getRunner(options.runner);
+
+    console.log('Running fixture: ' + fixture.key);
+
+    if (!runner) {
+        throw new Error('Runner "'  + runner + '" not found!');
+    }
 }
 
 module.exports = {
