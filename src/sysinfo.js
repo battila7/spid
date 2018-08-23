eca4c0fb1c8b2a7fe72e4eded3d8e93be760f7a5
Attila Bagossy <bagossyattila@outlook.com>
2018-08-23 09:51:24 +0200

    Created the sysinfo module wrapping systeminformation

---
+++ src/sysinfo.js
@@ -0,0 +1,54 @@
+const si = require('systeminformation');
+
+const sysinfo = {};
+
+(function addTime() {
+    const time = si.time();
+
+    sysinfo.time = {
+        current: time.current,
+        timezone: time.timezone
+    };
+})();
+
+const cpu = si.cpu()
+    .then(function addCpu(cpu) {
+        sysinfo.cpu = {
+            manufacturer: cpu.manufacturer,
+            brand: cpu.brand,
+            speed: cpu.speed,
+            cores: cpu.cores,
+            family: cpu.familiy,
+            model: cpu.model
+        };
+    });
+
+const mem = si.mem()
+    .then(function addMem(mem) {
+        sysinfo.memory = {
+            total: mem.total,
+            free: mem.free
+        };
+    })
+    .then(() => si.memLayout())
+    .then(function addMemLayout(memLayout) {
+        sysinfo.memory.layout = memLayout.map(mem => {
+            return {
+                size: mem.size,
+                type: mem.type,
+                clockSpeed: mem.clockSpeed
+            };
+        });
+    });
+
+const osInfo = si.osInfo()
+    .then(function addOs(osInfo) {
+        sysinfo.os = {
+            platform: osInfo.platform,
+            distro: osInfo.distro,
+            release: osInfo.release,
+            arch: osInfo.arch
+        };
+    });
+
+module.exports = () => Promise.all([ cpu, mem, osInfo ]).then(() => sysinfo);
