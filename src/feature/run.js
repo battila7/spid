53877b2c87619ea8d0c0039f5fd80f2e794d0a88
Attila Bagossy <bagossyattila@outlook.com>
2018-08-23 09:35:01 +0200

    New directory created on a per feature basis

--- src/feature/run.js
+++ src/feature/run.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const { mkdir } = require('fs-extra');
+
 const clone = require('./steps/clone');
 const cleanup = require('./steps/cleanup');
 const checkout = require('./steps/checkout');
@@ -18,18 +20,23 @@ function runFixtures({ description, fixtures }, feature, baseOptions) {
     return fixtures.reduce(reduceCallback, Promise.resolve());
 }
 
-function runFeature(feature, options) {
+function runFeature(feature, baseOptions) {
     console.log('\nBenchmarking feature: ' + feature.keys.join(' ') + '\n');
+
+    const featureResultDirectory =  feature.keys.join('_') + '_' + options.uuid;
+
+    const options = Object.assign({}, baseOptions, { resultDirectory: featureResultDirectory });
 
     const finallyFunc = () => cleanup(options.checkoutDirectory);
 
     return clone(options.remote, options.checkoutDirectory)
         .then(() => checkout(options.remote, feature.data.treeish, options.checkoutDirectory))
+        .then(() => mkdir(path.join(options.resultDirectory, featureResultDirectory)))
         .then(() => readFixtures(path.resolve(options.checkoutDirectory, feature.data.fixturesFile)))
         .then(fixtures => runFixtures(fixtures, feature, options))
         .then(() => finallyFunc().then(true))
         .catch(err => {
-            console.log('Error benchmarking feature');
+            console.log('Error benchmarking feature: ' + feature.keys.join(' '));
             console.log(err);
 
             return finallyFunc().then(false)
